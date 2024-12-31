2e1551e91023cdc98c5d92628420136760fec650
rajat0208 <rajatshrestha0208@gmail.com>
2024-12-31 22:39:04 +0545

    add task entiites using remult

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,8 @@
 import { remultNextApp } from "remult/remult-next";
+import { Task } from "./shared/Task"
 
-export const api = remultNextApp({});
+export const api = remultNextApp({
+    entities:[Task]
+});
+
+export const { POST, PUT, DELETE, GET } = api
