d28697df0bca29cde6f86e133ad18030841cbbf1
rajat0208 <rajatshrestha0208@gmail.com>
2025-01-01 21:00:03 +0545

    display the task list

---
+++ src/components/todo.tsx
@@ -0,0 +1,31 @@
+// src/components/todo.tsx
+
+'use client'
+import { useEffect, useState } from 'react'
+import { remult } from 'remult'
+import { Task } from '../shared/Task'
+
+const taskRepo = remult.repo(Task)
+
+export default function Todo() {
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  useEffect(() => {
+    taskRepo.find().then(setTasks)
+  }, [])
+  return (
+    <div>
+      <h1>Todos</h1>
+      <main>
+        {tasks.map((task) => {
+          return (
+            <div key={task.id}>
+              <input type="checkbox" checked={task.completed} />
+              {task.title}
+            </div>
+          )
+        })}
+      </main>
+    </div>
+  )
+}
