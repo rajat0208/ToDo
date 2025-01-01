21801a0a007823da2c3f848428362b644a3a9864
rajat0208 <rajatshrestha0208@gmail.com>
2025-01-01 21:25:54 +0545

    adding task, marking task, rename and save task

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,35 +1,71 @@
 // src/components/todo.tsx
 
 'use client'
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { remult } from 'remult'
 import { Task } from '../shared/Task'
 
 const taskRepo = remult.repo(Task)
 
 export default function Todo() {
-  const [tasks, setTasks] = useState<Task[]>([])
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [newTaskTitle, setNewTaskTitle] = useState("")
 
-  useEffect(() => {
-    taskRepo.find({
-        limit:20,
-        orderBy:{createdAt:"desc"},
-        //where:{completed:true}
-    }).then(setTasks)
-  }, [])
-  return (
-    <div>
-      <h1>Todos</h1>
-      <main>
-        {tasks.map((task) => {
-          return (
-            <div key={task.id}>
-              <input type="checkbox" checked={task.completed} />
-              {task.title}
-            </div>
-          )
-        })}
-      </main>
-    </div>
-  )
+    const addTask = async (e: FormEvent) => {
+        e.preventDefault()
+        try {
+            const newTask = await taskRepo.insert({ title: newTaskTitle })
+            setTasks([...tasks, newTask])
+            setNewTaskTitle("")
+        } catch (error: unknown) {
+            alert((error as { message: string }).message)
+        }
+    }
+
+
+    useEffect(() => {
+        taskRepo.find({
+            limit: 20,
+            orderBy: { createdAt: "desc" },
+            //where:{completed:true}
+        }).then(setTasks)
+    }, [])
+    return (
+        <div>
+            <h1>Todos</h1>
+            <main>
+                <form onSubmit={addTask}>
+                    <input
+                        value={newTaskTitle}
+                        placeholder='What needs to be done?'
+                        onChange={e => setNewTaskTitle(e.target.value)} />
+                    <button>ADD</button>
+                </form>
+                {tasks.map((task) => {
+                    const setTask = (value: Task) =>
+                        setTasks(tasks => tasks.map(t => (t === task ? value : t)))
+                    const setCompleted = async (completed: boolean) =>
+                        setTask(await taskRepo.save({ ...task, completed }))
+                    const setTitle = (title: string) => setTask({ ...task, title })
+                    const saveTask = async () => {
+                        try {
+                            setTask(await taskRepo.save(task))
+                        } catch (error: unknown) {
+                            alert((error as { message: string }).message)
+                        }
+                    }
+                    
+                    return (
+                        <div key={task.id}>
+                            <input type="checkbox" checked={task.completed} onChange={e => setCompleted(e.target.checked)} />
+                            <input value={task.title} onChange={e => setTitle(e.target.value)} />
+                            <button onClick={saveTask}>Save</button>
+                            
+                            {task.title}
+                        </div>
+                    )
+                })}
+            </main>
+        </div>
+    )
 }
