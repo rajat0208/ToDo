377cba5ea08794da30cf522c1ff740190e4f14f8
rajat0208 <rajatshrestha0208@gmail.com>
2025-01-03 21:11:48 +0545

    database

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,18 @@ import { remultNextApp } from "remult/remult-next";
 import { Task } from "./shared/Task"
 import { TasksController } from "./shared/TasksController";
 import { getUserOnServer } from "./auth"
+import { createPostgresDataProvider } from "remult/postgres"
+
+const DATABASE_URL = process.env["DATABASE_URL"]
 
 export const api = remultNextApp({
     entities:[Task],
     admin:true,
     controllers:[TasksController],
     getUser: getUserOnServer,
+    dataProvider: DATABASE_URL
+    ? createPostgresDataProvider({ connectionString: DATABASE_URL })
+    : undefined,
 });
 
 export const { POST, PUT, DELETE, GET } = api
